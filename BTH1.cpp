88ab05d9b37bc3c056a52675d38af5a494124df2
penroseDG <147390358+penroseDG@users.noreply.github.com>
2023-11-07 12:49:04 +0700

    Add files via upload

---
+++ BTH1.cpp
@@ -0,0 +1,20 @@
+//tai thu vien
+#include<stdio.h>
+#include<stdbool.h>//thu vien de dung duoc kieu du lieu bool
+//2. xay dung ham main de chay chuong trinh
+int main(){
+	//3, khai bao cac bien tuoi, gioi tinh, diem cua sinh vien
+	int age;
+	bool sex;
+	float mark;
+	//4 khoi tao cac bien su dung toan tu gan (=)
+	age = 18;
+	sex = true;//true Nam, false Nu
+	mark = 9.5;
+	//5, khai bao va khoi tao gia tri cho cac bien
+	char studentld[] = {'S','V','0','0','1'};//chuoi ki tu
+	char studentName[] = "Nguyen Van A";
+	//khoi bao va khoi tao cac bien number1, number2, number3
+	double number1 = 6.5, number2 = 8.2, number3 = -7.4;
+} 
+ 
